f70b9d100554120266e3191f25ab9cf486aa939c
sarathnayudunz <sarath.nayudu@ntcsoftech.com>
2016-06-03 15:55:46 +0530

    view and authentication modifications

---
+++ IppMvc3/Controllers/IBSQBController.cs
@@ -0,0 +1,37 @@
+﻿using IntuitSampleMVC.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntuitSampleMVC.Controllers
+{
+    public class IBSQBController : Controller
+    {
+        //
+        // GET: /IBSQB/
+
+        public ActionResult Index()
+        {
+            IBSQBCustomer obj = new IBSQBCustomer();
+
+            return View("../IBSInvoice/QBCustomers", obj.GetQBCustomers());
+        }
+
+        [HttpPost]
+        public ActionResult NewInvoice(string[] Selected)
+        {
+            //Generate Invoice
+            IBSQBInvoice inv = new IBSQBInvoice();
+            foreach (string custId in Selected)
+            {
+
+                inv.NewInvoice(custId);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ IppMvc3/Filters/AutoQBLoginFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace IntuitSampleMVC.Filters
+{
+    public class AutoQBLoginFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (WebSecurity.HasUserId)
+            {
+
+            }
+          
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
